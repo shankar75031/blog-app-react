b5b6d5b293f38ad938922479ed93614d5f076555
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 03:19:54 +0530

    added token to all api calls that need auth

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -51,7 +51,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch("http://localhost:8080/feed/posts?page=" + page)
+    fetch("http://localhost:8080/feed/posts?page=" + page, {
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
@@ -127,6 +131,9 @@ class Feed extends Component {
     fetch(url, {
       method: method,
       body: formData,
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
       // body: JSON.stringify({
       //   title: postData.title,
       //   content: postData.content,
@@ -188,6 +195,9 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     fetch("http://localhost:8080/feed/post/" + postId, {
       method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
