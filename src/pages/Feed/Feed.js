425f8cfc9d55733972efd1d4656b193ad1d9e02c
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 02:15:48 +0530

    edit post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -61,7 +61,12 @@ class Feed extends Component {
       .then((resData) => {
         console.log(resData);
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map((post) => {
+            return {
+              ...post,
+              imagePath: post.imageUrl,
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false,
         });
@@ -115,7 +120,8 @@ class Feed extends Component {
     let url = "http://localhost:8080/feed/post";
     let method = "POST";
     if (this.state.editPost) {
-      url = "URL";
+      url = "http://localhost:8080/feed/post/" + this.state.editPost._id;
+      method = "PUT";
     }
 
     fetch(url, {
