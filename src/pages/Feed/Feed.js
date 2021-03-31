a591c8d46d27811b9fc1fdfc0471ed8bd6d967b1
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-31 21:10:49 +0530

    integrated socket io for realtime update of posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -41,7 +41,28 @@ class Feed extends Component {
     const socket = io("http://localhost:8080", {
       transports: ["websocket", "polling", "flashsocket"],
     });
+    socket.on("posts", (data) => {
+      if (data.action === "create") {
+        this.addPost(data.post);
+      }
+    });
   }
+
+  addPost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
 
   loadPosts = (direction) => {
     if (direction) {
